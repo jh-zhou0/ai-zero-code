ff105b654f8c770428e87fe4bc7f5671ca5b1ee2
kayson <1512090646@qq.com>
2026-06-06 13:02:34 +0800

    feat(home): 配置中文本地化

--- ai-zero-code-fronted/src/App.vue
+++ ai-zero-code-fronted/src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <BasicLayout />
+  <a-config-provider :locale="zhCN">
+    <BasicLayout />
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+
+dayjs.locale('zh-cn')
 </script>
--- ai-zero-code-fronted/src/views/HomeView.vue
+++ ai-zero-code-fronted/src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import { check } from '@/api/healthController.ts'
+import { ref } from 'vue'
+const open = ref<boolean>(false)
+
+const showModal = () => {
+  open.value = true;
+}
+
+const handleOk = (e: MouseEvent) => {
+  console.log(e);
+  open.value = false;
+}
 
 check().then((res) => {
   console.log(res)
@@ -9,5 +20,13 @@ check().then((res) => {
 
 <template>
   <main>
+    <div>
+      <a-button type="primary" @click="showModal">Open Modal</a-button>
+      <a-modal v-model:open="open" title="Basic Modal" @ok="handleOk">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </a-modal>
+    </div>
   </main>
 </template>
